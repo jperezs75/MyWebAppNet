1bf63016b23ccba2645e2a7f1edf9e43031d2890
jperezs75 <47405882+jperezs75@users.noreply.github.com>
2019-10-28 10:18:34 -0600

    Add files via upload

---
+++ MyWebApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            this.ViewBag.HostName = System.Environment.MachineName;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
